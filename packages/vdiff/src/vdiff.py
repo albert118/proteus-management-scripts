00cde42a81fc7828fefa46ba3e7a4a9efddcc8b2
Albert Ferguson <albertferguson118@gmail.com>
2026-08-14 17:54:53 +1000

    README updated

--- packages/vdiff/src/vdiff.py
+++ packages/vdiff/src/vdiff.py
@@ -1,47 +1,11 @@
 #!/usr/bin/env python3
 
 """
-Check given video stream for issues using a few strategies. This works for lossy re-encodes
-by checking the result against a snapshot of the original file. 
-
-This calculates the following stats:
-- Netflix's VMAF (https://en.wikipedia.org/wiki/Video_Multimethod_Assessment_Fusion)
-- PSNR (peak signal to noise ratio)
-- Structural Similarity Index Measure (SSIM)
-The (VMAF) score of is betwen [0, 100]
-- 93 - 95 is imperceptible to the human eye
-- 100 is perfect (unlikely).
-
-The PSNR is decible scale
-higher is better
-- relies on MSE
-- a standard, 8-bit, image with a "good" score would be ~[30, 50] db
-
-SSIM compares some other common visuals,
-- luminance, contrast, and structure
-- not captured by other stats
-- [0.0, 1.0] range. 1.0 is identical to original, and 0.0 is no similarity
-
-This script uses ffmpeg to calculate the various stats that factor into the VMAF score. This in turn relise on having the libvmaf filter compiled with it.
-This is not standard typically! However, there are other precompiled binaries that can be used.
-
-```sh
-wget https://johnvansickle.com/ffmpeg/builds/ffmpeg-git-amd64-static.tar.xz
-tar -xf ffmpeg-git-amd64-static.tar.xz
-cd ffmpeg-*-amd64-static/
-# test if the filter is available
-./ffmpeg -filters | grep vmaf
-```
+A tool to help validate video stream re-encode quality without manually checking them by eye.
+This check the given video stream for issues using a few strategies.
+It works for lossy re-encodes by checking the result against a snapshot of the original file.
 """
 
-
-# TODO: [ ] confirm linear fallback mode
-# TODO: [ ] confirm parallel processing isn't skipping frames (test batch splitting)
-# TODO: [ ] cancelling parallel processing leaves hanging ffmpeg processes!!! Need to clean these up correctly
-# TODO: [ ] test with different number of workers provisioned
-# TODO: [ ] TQDM fallback/disable within docker containers
-# TODO: [ ] time container alignment to reduce processing overhead
-# TODO: [ ] testing!
 
 import concurrent.futures
 import subprocess
